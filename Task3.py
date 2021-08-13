1b044b84e099e3aad2a746f01bbab0e670115235
A7madhatem <a.h.aboelkhair@gmail.com>
2021-08-13 14:01:38 +0200

    Task3

---
+++ Task3.py
@@ -0,0 +1,12 @@
+def dev(number):
+    print("The numbers that are divisible ")
+    for i in list:
+        if i % number == 0:
+         print(i)
+n = int(input("Enter the number : "))
+e = int(input("Enter the number of elements in the list : "))
+list = []
+for i in range(0, e):
+    ele = int(input("Enter the elements"))
+    list.append(ele)
+dev(n)
